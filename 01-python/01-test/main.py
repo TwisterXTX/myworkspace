f15a3f1157d86e74a3f3f8482b1fb4a55bed8155
xitianxiang <xtx_611@126.com>
2020-03-15 14:18:02 +0800

    & simple

--- 01-python/01-test/main.py
+++ 01-python/01-test/main.py
@@ -11,6 +11,9 @@ print('hello world')
 print('hello world')
 
 print('creat a new branch is quick')
+
+print('Creating a new branch is quick & simple')
+
 img = cv2.imread('bird.jpg')
 
 cv2.imshow('imshow', img)
